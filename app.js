90a023ca025669109b1af1b5d3640196d89abdb8
adityas567 <adityas@esspl.com>
2022-10-07 15:32:07 +0530

    new login file

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 
-
-
-
-
-
-
-function loginValidation()
+function loginValidation(e)
 {
-   
+    e.preventDefault();
     let email=document.forms.loginForm.email.value;
     let password=document.forms.loginForm.password.value;
     let emailHelp=document.querySelector('#emailHelp');
@@ -23,3 +17,26 @@ function loginValidation()
         passwordHelp.innerHTML="Please Enter a Valid Password"
     }
 }
+
+//how to validate a login form?
+<form name="loginform" onSubmit="return validateForm();" action="main.html" method="post">
+        
+        </form>
+        
+        <script>
+            function validateForm() {
+                var un = document.loginform.usr.value;
+                var pw = document.loginform.pword.value;
+                var username = "username"; 
+                var password = "password";
+                if ((un == username) && (pw == password)) {
+                    return true;
+                }
+                else {
+                    alert ("Login was unsuccessful, please check your username and password");
+                    return false;
+                }
+          }
+        </script>
+
+
